24f62b781f18d402fc283f1afe8f815168cee73c
Ayan Ahmad <ayan.ahmad03600@gmail.com>
2026-05-27 02:09:47 +0530

    Add files via upload

---
+++ firstProgram.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+   
+     // This is a single comment in C language
+
+     /*This is multiline comment in C language*/
+
+    // char name[] = "Ayan ahmad";
+    // printf("%s", name);
+    // // return 0;
+
+    // int a = 8, b = 10;
+    // printf("Additions: %d\n", a+b);
+    int a = 87, b = 89;
+
+    float pi = 3.14;
+    char c = 'A';
+    printf("This is integer value %d\n", a, b);
+    // printf("This is also a integer value %d\n", d);
+    printf("This is a character value %c\n", c);
+    printf("This is float value %f\n", pi);
+    printf("I'm learning C with myself!!");
+    return 0;
+
+}
+
+
